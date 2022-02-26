723ecf43b810fe8786f097863748bbc39e6cde81
KingMeeko <92309055+KingMeeko@users.noreply.github.com>
2022-02-26 13:31:20 -0500

    Add files via upload

--- ex00/main.c
+++ ex00/main.c
@@ -5,15 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/26 12:03:45 by dantremb          #+#    #+#             */
-/*   Updated: 2022/02/26 12:17:04 by dantremb         ###   ########.fr       */
+/*   Created: 2022/02/24 17:02:12 by dantremb          #+#    #+#             */
+/*   Updated: 2022/02/25 13:10:55 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(void)
+void	ft_putchar(char a)
 {
-	rush(5, 5);
-	return (0);
+	write(1, &a, 1);
+}
+
+void	ft_putnbr(int nb)
+	{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+
+
+int	main(void)
+	{
+	ft_putnbr(4343);
 }
