7d26af6fb36923a23b0c149d4a9e498749de46f0
KingMeeko <92309055+KingMeeko@users.noreply.github.com>
2022-02-26 08:45:26 -0500

    Got time to make the function ready be code :D

--- ex00/main.c
+++ ex00/main.c
@@ -4,28 +4,31 @@ void    ft_putchar(char c)
     {
     write(1, &c, 1);
 }
-
-void    ft_print_table(int len, int hei)
+int ft_choose_char(int lengh, int height)
 {
     char o;
     char l;
     char t;
     char s;
-    int i;
-    int ii;
 
     l = 124;
     o = 111;
     t = 45;
     s = 32;
-    ii = 0;
+    ft_putchar(o);
+}
+void    ft_print_table(int len, int hei)
+{
+    int i;
+    int ii;
 
+    ii = 0;
     while (ii < hei)
     {
         i = 0;
         while (i < len)
         {
-            ft_putchar(o);
+            ft_choose_char(i, ii);
             i++;
         }
         ft_putchar(10);
